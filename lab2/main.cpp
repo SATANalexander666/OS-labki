ca5a5ad252b4d4a09dd8e08e57e804ff6400eb9b
dissatisfied-nerd <sanek.glov@gmail.com>
2022-09-24 23:10:21 +0300

    adding make script

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,9 +1,45 @@
 #include<iostream>
-#include<utils.h>
+#include<string>
+
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+std::string Parent()
+{
+    std::string str1;
+
+    std::cin >> str1;
+    
+    return str1;
+}
+
+void Child1(std::string str)
+{
+    std::cout << str << std::endl;
+}
+
+void Child2(std::string str)
+{
+    std::cout << str << std::endl;
+}
 
 int main()
 {
-    
+    std::string str;
+
+    int pid = fork();
+
+    if (pid > 0)
+    {
+        std::cout << "Parent " << std::endl;
+        str = Parent();
+    }
+    else if (pid == 0)
+    {
+        std::cout << "Child: ";
+        Child1(str);
+    }
 
     return 0;
 }
