d575aa7ad87756826f74e3125c4a17e98c836536
SATANalexander666 <10b1111nika@gmail.com>
2022-11-13 15:28:46 +0300

    deleting build added by accident

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/axr/prog/OS/main
-# Build directory: /home/axr/prog/OS/main/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lab2")
-subdirs("lab3")
-subdirs("tests")
